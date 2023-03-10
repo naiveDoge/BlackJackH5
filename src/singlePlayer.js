a17d5508f52832c757fbdbe851314eee3708dd8a
Zhou <zhoushangren@yahoo.com>
2023-03-10 11:18:49 +0800

    function of generating the cards

--- src/singlePlayer.js
+++ src/singlePlayer.js
@@ -10,16 +10,42 @@ var gameData = {
 $('#buttonStart').click(function(){
     $('#cardWelcome').css('display', 'none');
     $('#cardGaming').css('display', 'block');
+    gameInit();
 });
 
 function gameInit() {
-
+    gameData['dealer'] = [];
+    gameData['dealer'].push(generateCard());
+    gameData['dealer'].push(generateCard());
+    addCardToDOM('dealersCards', gameData['dealer'][0]);
+    
+    //add a back of the card
+    $('#dealersCards').append(Poker.getBackCanvas(120, '#7A7BB8', '#2E319C'));
 }
 
+function addCardToDOM(elementID, card) {
+    $('#' + elementID).append(Poker.getCardCanvas(120, generateCardFace(), getCardFromNum(card)));
+}
+
+
 function generateCard() {
-    var cardNum = getRandomInt() + 1;
+    return getRandomInt(13) + 1;
+}
+
+function getCardFromNum(cardNum) {
+    cardNum = cardNum.toString();
+    cardNum = cardNum.replace('11', 'J');
+    cardNum = cardNum.replace('12', 'Q');
+    cardNum = cardNum.replace('13', 'K');
+    cardNum = cardNum.replace('1', 'A');
+    return cardNum;
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
+}
+
+function generateCardFace() {
+    var faces = ['h', 'd', 's', 'c'];
+    return faces[getRandomInt(4)];
 }
